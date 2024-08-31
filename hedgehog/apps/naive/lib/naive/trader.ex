e5869e9c979c37093fc3df8c6e73883b7a66189a
Gianni-Lauritz Grubert <animalibera@mail.de>
2024-08-31 17:23:14 +0200

    End of chapter 2

--- hedgehog/apps/naive/lib/naive/trader.ex
+++ hedgehog/apps/naive/lib/naive/trader.ex
@@ -1,10 +1,23 @@
-# /apps/naive/lib/naive/trader.ex
 defmodule Naive.Trader do
   use GenServer
 
+  alias Decimal, as: D
+  alias Streamer.Binance.TradeEvent
+
   require Logger
 
-  def start_link(args) do
+  defmodule State do
+    @enforce_keys [:symbol, :profit_interval, :tick_size]
+    defstruct [
+      :symbol,
+      :buy_order,
+      :sell_order,
+      :profit_interval,
+      :tick_size
+    ]
+  end
+
+  def start_link(%{} = args) do
     GenServer.start_link(__MODULE__, args, name: :trader)
   end
 
@@ -23,6 +36,90 @@ defmodule Naive.Trader do
      }}
   end
 
+  def handle_cast(
+        %TradeEvent{price: price},
+        %State{symbol: symbol, buy_order: nil} = state
+      ) do
+    quantity = "10"
+
+    Logger.info("Placing BUY order for #{symbol} @ #{price}, quantity: #{quantity}")
+
+    {:ok, %Binance.OrderResponse{} = order} =
+      Binance.order_limit_buy(symbol, quantity, price, "GTC")
+
+    {:noreply, %{state | buy_order: order}}
+  end
+
+  def handle_cast(
+        %TradeEvent{
+          buyer_order_id: order_id,
+          quantity: quantity
+        },
+        %State{
+          symbol: symbol,
+          buy_order: %Binance.OrderResponse{
+            price: buy_price,
+            order_id: order_id,
+            orig_qty: quantity
+          },
+          profit_interval: profit_interval,
+          tick_size: tick_size
+        } = state
+      ) do
+    sell_price = calculate_sell_price(buy_price, profit_interval, tick_size)
+
+    Logger.info(
+      "Buy order filled, placing SELL order for " <>
+        "#{symbol} @ #{sell_price}, quantity: #{quantity}"
+    )
+
+    {:ok, %Binance.OrderResponse{} = order} =
+      Binance.order_limit_sell(symbol, quantity, sell_price, "GTC")
+
+    {:noreply, %{state | sell_order: order}}
+  end
+
+  def handle_cast(
+        %TradeEvent{
+          seller_order_id: order_id,
+          quantity: quantity
+        },
+        %State{
+          sell_order: %Binance.OrderResponse{
+            order_id: order_id,
+            orig_qty: quantity
+          }
+        } = state
+      ) do
+    Logger.info("Trade finished, trader will now exit")
+    {:stop, :normal, state}
+  end
+
+  def handle_cast(%TradeEvent{}, state) do
+    {:noreply, state}
+  end
+
+  defp calculate_sell_price(buy_price, profit_interval, tick_size) do
+    fee = "1.001"
+    original_price = D.mult(buy_price, fee)
+
+    net_target_price =
+      D.mult(
+        original_price,
+        D.add("1.0", profit_interval)
+      )
+
+    gross_target_price = D.mult(net_target_price, fee)
+
+    D.to_string(
+      D.mult(
+        D.div_int(gross_target_price, tick_size),
+        tick_size
+      ),
+      :normal
+    )
+  end
+
   defp fetch_tick_size(symbol) do
     Binance.get_exchange_info()
     |> elem(1)
@@ -32,16 +129,4 @@ defmodule Naive.Trader do
     |> Enum.find(&(&1["filterType"] == "PRICE_FILTER"))
     |> Map.get("tickSize")
   end
-
-end
-
-defmodule State do
-  @enforce_keys [:symbol, :profit_interval, :tick_size]
-  defstruct [
-    :symbol,
-    :buy_order,
-    :sell_order,
-    :profit_interval,
-    :tick_size
-  ]
 end
